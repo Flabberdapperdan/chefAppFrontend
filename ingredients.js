d2f3031c3065c35faede2b01dfb82f45c68024c1
Joost de Bree <joostdebree@outlook.com>
2022-06-14 13:47:53 +0200

    Added pagenation, sorting and ordering

--- ingredients.js
+++ ingredients.js
@@ -1,73 +1,219 @@
 import {keys} from "./keys.js";
 
 const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
+const queryParams = new URLSearchParams(queryString);
+
+const baseApiUrl = keys.url + "api/ingredients";
+
+let jsonObject;
+let page = 0;
+let size = 10;
+let sortBy = "id";
+let orderBy = "asc";
+
+const template = {
+  "header":{'<>':'thead', 'id':'table-header', 'html':[
+    {'<>':'tr', 'html':[
+      {'<>':'th', 'text':'Id', 'onclick':function(){
+        applySortAndOrder("id");
+        page = 0;
+        readBody(true);
+      }},
+      {'<>':'th', 'text':'Code', 'onclick':function(){
+        applySortAndOrder("code");
+        page = 0;
+        readBody(true);
+      }},
+      {'<>':'th', 'text':'Group', 'onclick':function(){
+        applySortAndOrder("group");
+        page = 0;
+        readBody(true);
+      }},
+      {'<>':'th', 'text':'Name', 'onclick':function(){
+        applySortAndOrder("name");
+        page = 0;
+        readBody(true);
+      }},
+      {'<>':'th', 'text':'Market Price', 'onclick':function(){
+        applySortAndOrder("marketprice");
+        page = 0;
+        readBody(true);
+      }},
+      {'<>':'th', 'html':[{'<>':'select', 'html':[
+          {'<>':'option', 'value':'10', 'text':'10'},
+          {'<>':'option', 'value':'20', 'text':'20'},
+          {'<>':'option', 'value':'30', 'text':'30'},
+          {'<>':'option', 'value':'40', 'text':'40'},
+          {'<>':'option', 'value':'50', 'text':'50'},
+          {'<>':'option', 'value':'100', 'text':'100'},
+      ], 'onchange':function(){
+        page = 0;
+        size = this.val();
+        readBody(true);
+      }}]},
+    ]},
+  ]},
+  "body":{'<>':'tbody', 'id':'table-body', 'html':[
+    {'<>':'tr', 'data-id':'${id}', 'html':[
+      {'<>':'td', 'text':'${id}'},
+      {'<>':'td', 'text':'${code}'},
+      {'<>':'td', 'text':'${group}'},
+      {'<>':'td', 'text':'${name}'},
+      {'<>':'td', 'text':'${marketPrice}'},
+      {'<>':'td', 'html':[{'<>':'span', 'class':'material-symbols-outlined', 'text':'delete', 'onclick':function(evObject){
+        deleteObject(evObject.obj.id);
+      }}]},
+    ], '{}':function(){return(this.ingredients)}},
+  ]},
+  "footer":{'<>':'tfoot', 'id':'table-footer', 'html':[
+    {'<>':'tr', 'html':[
+      {'<>':'td'},
+      {'<>':'td', 'html':[{'<>':'input', 'type':'text', 'data-name':'code'}]},
+      {'<>':'td', 'html':[{'<>':'input', 'type':'text', 'data-name':'group'}]},
+      {'<>':'td', 'html':[{'<>':'input', 'type':'text', 'data-name':'name'}]},
+      {'<>':'td', 'html':[{'<>':'input', 'type':'text', 'data-name':'marketPrice'}]},
+      {'<>':'td', 'html':[{'<>':'span', 'class':'material-symbols-outlined', 'text':'add', 'onclick':function(){
+        createObject();
+      }}]},
+    ]},
+    {'<>':'tr', 'html':[
+      {'<>':'td', 'html':[{'<>':'span', 'class':'material-symbols-outlined', 'text':'keyboard_double_arrow_left', 'onclick':function(){
+        page = 0;
+        readBody(true);
+      }, 'style':function(){
+        if(page == 0)
+        {
+          return "visibility:hidden";
+        }
+      }}]},
+      {'<>':'td', 'html':[{'<>':'span', 'class':'material-symbols-outlined', 'text':'chevron_left', 'onclick':function(){
+        page--;
+        readBody(true);
+      }, 'style':function(){
+        if(page == 0)
+        {
+          return "visibility:hidden";
+        }
+      }}]},
+      {'<>':'td', 'text':'Page ${currentPage} of ${totalPages}'},
+      {'<>':'td', 'html':[{'<>':'span', 'class':'material-symbols-outlined', 'text':'chevron_right', 'onclick':function(){
+        page++;
+        readBody(true);
+      }, 'style':function(dataObject){
+        if(page == dataObject.totalPages - 1)
+        {
+          return "visibility:hidden";
+        }
+      }}]},
+      {'<>':'td', 'html':[{'<>':'span', 'class':'material-symbols-outlined', 'text':'keyboard_double_arrow_right', 'onclick':function(evObject){
+        page = evObject.obj.totalPages - 1;
+        readBody(true);
+      }, 'style':function(dataObject){
+        if(page == dataObject.totalPages - 1)
+        {
+          return "visibility:hidden";
+        }
+      }}]},
+    ]}
+  ]},
+};
+
+function init()
+{
+  if(queryParams.has('page'))
+  {
+    page = queryParams.get('page');
+  }
+  if(queryParams.has('size'))
+  {
+    size = queryParams.get('size');
+  }
+  readTable(true);
+}
 
 /*
 Get (read) all ingredients
 */
-const readObjects = async () => {
-  let apiUrl = keys.url + "api/ingredients"
-
-  const apiParams = [];
-  for (const key of urlParams.keys())
-  {
-    apiParams.push(key + '=' + urlParams.get(key));
-  }
-
-  apiUrl += '?' + apiParams.join('&');
-
+const fetchObject = async() => {
+  let apiUrl = baseApiUrl + `?page=${page}&size=${size}&sort_by=${sortBy}&order_by=${orderBy}`;
   let response = await fetch(apiUrl);
-  console.log(response);
-  let jsonObject = await response.json();
-  let jsonArray = jsonObject["ingredients"];
-  console.log(jsonArray);
+  return response.json();
+}
 
-  let headerTemplate = {'<>':'thead', 'html':[
-    {'<>':'tr', 'html':[
-      {'<>':'th', 'text':'Id'},
-      {'<>':'th', 'text':'Code'},
-      {'<>':'th', 'text':'Group'},
-      {'<>':'th', 'text':'Name'},
-      {'<>':'th', 'text':'Market Price'},
-    ]}
-  ]};
+function applySortAndOrder(value)
+{
+  if(sortBy == value)
+  {
+    if(orderBy == "asc")
+    {
+      orderBy = "desc";
+    }
+    else
+    {
+      orderBy = "asc";
+    }
+  }
+  else
+  {
+    sortBy = value;
+    orderBy = "asc";
+  }
+}
 
-  let bodyTemplate = {'<>': 'tr', 'data-id':'${id}' ,'html':[
-    {'<>':'td', 'text':'${id}'},
-    {'<>':'td', 'text':'${code}'},
-    {'<>':'td', 'text':'${group}'},
-    {'<>':'td', 'text':'${name}'},
-    {'<>':'td', 'text':'${marketPrice}'},
-    /*{'<>': 'td', 'html':[
-      {'<>': 'span', 'text': '${id}: ${code} x ${quantity}', '{}':function(){return(this.nutrients)}}
-    ]},*/
-    {'<>':'td', 'html':[
-      {'<>':'button', 'html':'Edit', 'class':'object-edit','onclick':function(obj,index){
-        editObject(obj);
-      }},
-    ]},
-  ]};
+const readTable = async (fetch = false) => {
+  if(fetch)
+  {
+    jsonObject = await fetchObject();
+  }
+  console.log(jsonObject);
 
-  let footerTemplate = {'<>': 'tfoot', 'html':[
-    {'<>': 'td', 'html':[{'<>':'input', 'type':'text', 'data-name':'id'}]},
-    {'<>': 'td', 'html':[{'<>':'input', 'type':'text', 'data-name':'code'}]},
-    {'<>': 'td', 'html':[{'<>':'input', 'type':'text', 'data-name':'group'}]},
-    {'<>': 'td', 'html':[{'<>':'input', 'type':'text', 'data-name':'name'}]},
-    {'<>': 'td', 'html':[{'<>':'input', 'type':'text', 'data-name':'marketPrice'}]},
-    {'<>': 'td', 'html':[{'<>':'button', 'id':'object-create', 'data-name':'marketPrice'}]},
-  ]}
+  readHeader();
+  readBody();
+}
+
+const readHeader = async (fetch = false) => {
+  if(fetch)
+  {
+    jsonObject = await fetchObject();
+  }
+  console.log(jsonObject);
   
-  $("#ingredient-table").json2html(jsonArray[0], headerTemplate);
-  $("#ingredient-table").json2html(jsonArray, bodyTemplate);
-  $("#ingredient-table").json2html(jsonArray[0], footerTemplate);
+  $("#table-header").json2html(jsonObject, template.header, {method:"replace"});
+}
+
+const readBody = async (fetch = false) => {
+  if(fetch)
+  {
+    jsonObject = await fetchObject();
+  }
+  console.log(jsonObject);
+
+  $("#table-body").json2html(jsonObject, template.body, {method:"replace"});
+  $("#table-footer").json2html(jsonObject, template.footer, {method:"replace"});
 }
 
 /*
 Post (create) an ingredient
 */
 
-const createObject = async (objectTemplate) => {
+const createObject = async () => {
+  let body = {};
+  body['code'] = document.querySelector('[data-name="code"]').value;
+  body['group'] = document.querySelector('[data-name="group"]').value;
+  body['name'] = document.querySelector('[data-name="name"]').value;
+  body['marketPrice'] = document.querySelector('[data-name="marketPrice"]').value;
+  let response = await fetch(keys.url + "api/ingredients", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body),
+  });
+  let jsonObject = await response.json();
+  readBody(true);
+}
+
+/* const createObject = async (objectTemplate) => {
   let body = {};
   let nutrientIds = [];
   for(let i in objectTemplate.children)
@@ -113,7 +259,7 @@ const createObject = async (objectTemplate) => {
     let jsonObject2 = await linkResponse.json();
   }
   //location.reload();
-}
+} */
 
 /*
 Put (update) an ingredient
@@ -201,9 +347,9 @@ const deleteObject = async (objectId) => {
   let response = await fetch(keys.url + `api/ingredients/${objectId}`, {
     method: 'DELETE'
   });
-  location.reload();
+  readBody(true);
 }
 
-window.onload = readObjects;
+window.onload = init;
 
 // Via NPM of Nodeman hosten-inladen
